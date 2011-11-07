5e0860b5d268ecb951ecaeeac19609455b467943
Chris Barr <chris.barr@ntlworld.com>
2011-11-07 12:35:30 +0000

    Created /demo folder and started list_topics file

---
+++ demo/list_topics.php
@@ -0,0 +1,8 @@
+<?php
+
+include('./config.inc.php');
+include('../lib/amazonsns.class.php');
+
+$SNS = new AmazonSNS(AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY);
+
+print_r($SNS->listTopics());
