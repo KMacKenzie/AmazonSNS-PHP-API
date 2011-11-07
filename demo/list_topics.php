70786b04a97553010d68d4bade5a9edb5d7d8978
Chris Barr <chris.barr@ntlworld.com>
2011-11-07 13:01:39 +0000

    Work on list_topics

--- demo/list_topics.php
+++ demo/list_topics.php
@@ -5,4 +5,9 @@ include('../lib/amazonsns.class.php');
 
 $SNS = new AmazonSNS(AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY);
 
-print_r($SNS->listTopics());
+$topics = $SNS->listTopics()->member;
+
+foreach($topics as $topic)
+{
+	print_r($topic);
+}
