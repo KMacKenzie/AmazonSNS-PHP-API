0e0032cee679621a0fe92595cd69299e574f7e42
Chris Barr <chris.barr@ntlworld.com>
2011-11-09 21:33:00 +0000

    Created show topic page

---
+++ demo/show_topic.php
@@ -0,0 +1,23 @@
+<?php
+
+include('./config.inc.php');
+include('../lib/amazonsns.class.php');
+
+$SNS = new AmazonSNS(AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY);
+
+$topicArn = $_GET['topic'];
+
+$topic = $SNS->getTopicAttributes($topicArn);
+
+echo '<h1>Topic</h1>';
+
+print_r($topic);
+
+echo '<h2>Subscriptions</h2>';
+
+$subs = $SNS->getSubscriptionsByTopic($topicArn);
+
+foreach($subs as $sub)
+{
+	print_r($sub);
+}
